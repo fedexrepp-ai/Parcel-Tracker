1d785b0a6295405657d257391c76411cb275f1d6
fedexrepp-ai <fedexrepp@gmail.com>
2026-07-14 06:17:04 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,4 +1,19 @@
 import { auth } from "./firebase.js";
+
+import {
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+
+    if (!user) {
+        window.location.href = "login.html";
+    }
+
+});
+
+
+import { auth } from "./firebase.js";
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 
 const email = document.getElementById("email");
